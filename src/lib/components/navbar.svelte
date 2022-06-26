8e9934b78d79c47bd11418fc6b702995ce10188e
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-26 15:01:58 +0200

    reorganized general layout, navbar component

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let currentLetter: string;
+	export let letters: Array<string>;
+
+	if (!letters.includes(currentLetter)) {
+		throw new Error('InvalidCurrentLetter');
+	}
+</script>
+
+<!--  -->
+
+<div
+	class="
+			sticky top-0 px-4 py-2 space-x-4 bg-white
+			flex flex-row flex-nowrap items-center justify-center
+"
+>
+	<!-- Border sx -->
+	<hr class="border-[1px] border-red-600 grow" />
+
+	<!-- Letters -->
+	<div class="space-x-1 flex flex-row flex-nowrap items-center justify-center">
+		{#each letters as letter}
+			<a
+				href="#{letter}"
+				class="
+					w-10 h-10 rounded-lg
+					flex flex-row flex-nowrap items-center justify-center
+					{currentLetter == letter ? 'bg-red-600' : 'border-2 border-red-600'}
+			"
+			>
+				<p
+					class="
+						font-mono 
+						{currentLetter == letter ? 'text-white' : 'text-red-600'}
+				"
+				>
+					{letter.toUpperCase()}
+				</p>
+			</a>
+		{/each}
+	</div>
+
+	<!-- Border dx -->
+	<hr class="border-[1px] border-red-600 grow" />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,10 @@
 	import _ from 'lodash';
 	import json from '$lib/labels-list.json';
 	import { assets } from '$app/paths';
+
 	import IntersectionObserver from '$lib/components/IntersectionObserver.svelte';
 	import Image from '$lib/components/Image.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
 
 	// Getting filenames
 	const filenames: Array<string> = json;
@@ -27,47 +29,24 @@
 
 <!--  -->
 
-<div
-	class="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4 pb-8 pt-0 items-stretch"
->
-	{#each peroni as letter}
-		<!-- Getting letter group -->
-		{@const labels = groups[letter]}
+{#each peroni as letter}
+	<!-- Getting letter group -->
+	{@const labels = groups[letter]}
 
-		<div id={letter} class="col-span-full h-1 mb-[-1rem]" />
-		<div
-			class="
-				mt-[-1rem]
-				py-2 space-x-4 col-span-full bg-white
-				flex flex-row flex-nowrap items-center justify-center
-				self-start sticky top-0"
-		>
-			<hr class="border-[1px] border-red-600 grow" />
-			<div class="space-x-1 flex flex-row flex-nowrap items-center justify-center">
-				{#each peroni as letterIndex}
-					<a
-						href="#{letterIndex}"
-						class="
-					 w-10 h-10 rounded-lg
-					flex flex-row flex-nowrap items-center justify-center
-					{letter == letterIndex ? 'bg-red-600' : 'border-2 border-red-600'}
-				"
-					>
-						<p
-							class="
-						font-mono 
-						{letter == letterIndex ? 'text-white' : 'text-red-600'}
-					"
-						>
-							{letterIndex.toUpperCase()}
-						</p>
-					</a>
-				{/each}
-			</div>
-			<hr class="border-[1px] border-red-600 grow" />
-		</div>
+	<!-- Anchor -->
+	<div id={letter} class="block h-0" />
 
-		<!-- Drawing labels -->
+	<!-- Navbar -->
+	<Navbar letters={peroni} currentLetter={letter} />
+
+	<!-- Content -->
+	<div
+		class="
+			container mx-auto p-4 mb-12
+			grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 items-stretch
+	"
+	>
+		<!-- Labels -->
 		{#if labels}
 			{#each labels as label}
 				<div class="flex flex-col flex-nowrap justify-between">
@@ -79,7 +58,7 @@
 								<Image src={`${assets}/${label.file}`} alt={label.word} />
 								{/if}
 							</IntersectionObserver> -->
-					<p class="bg-gray-200 font-mono p-2 text-center">{label.word}</p>
+					<p class="font-mono p-2 text-center">{label.word}</p>
 				</div>
 			{/each}
 		{/if}
@@ -88,5 +67,5 @@
 		{#if !labels || !labels.length}
 			<p class="font-mono text-gray-400">No data</p>
 		{/if}
-	{/each}
-</div>
+	</div>
+{/each}
